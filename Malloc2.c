ce6ccdcb71d0ede81b336875473f7f0aa3dadeb1
Andreas Gomes Marchi <andreasgm38@gmail.com>
2024-10-15 22:16:08 -0300

    Adição de funções Matriz e Malloc

--- Malloc2.c
+++ Malloc2.c
@@ -5,11 +5,11 @@ int main (void){
 int *sequenciaValores = (int *)malloc(sizeof(int) * 3);
 int *p = sequenciaValores; // Referencia p
 
-*(p + 1) = 20; // Atribui 20 ao primeiro endereço dinâmico
+*(p + 1) = 20; // Atribui 20 ao primeiro endereço dinâmico 1
 
-*(sequenciaValores) = 40; // Atribui 40 ao ponteiro seq
+*(sequenciaValores) = 40; // Atribui 40 ao ponteiro seq 0
 
-*(p + 2) = *(sequenciaValores) * 2; //Atribui o dobro do valor de seq no segundo endereço alocado
+*(p + 2) = *(sequenciaValores) * 2; //Atribui o dobro do valor de seq no segundo endereço alocado 2
 
 printf("%d\n", *(p + 2));
 
